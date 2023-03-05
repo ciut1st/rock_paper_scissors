6b16e032fdd62a54f3f29266cfecc62be9c62f2c
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-06 10:48:05 +1100

    tough

--- script.js
+++ script.js
@@ -8,15 +8,28 @@ function getComputerChoice() {
     return compChoice[rand]
 }
 
+let computerSelection = getComputerChoice()
+
 // Getting data from the HTML
 const rockButton = document.querySelector(".rock")
 const paperButton = document.querySelector(".paper")
 const scissorsButton = document.querySelector(".scissors")
 
 
-
-// Start the game
-const computerSelection = getComputerChoice()
-// const playerSelection = 
-
-// console.log(playRound(playerSelection, computerSelection))
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection == "scissors"
+    || playerSelection == "paper" && computerSelection == "rock"
+    || playerSelection == "scissors" && computerSelection == "paper") {
+        return "Win"
+    } else if (
+        playerSelection == "rock" && computerSelection == "paper"
+        || playerSelection == "paper" && computerSelection == "scissors"
+        || playerSelection == "scissors" && computerSelection == "rock") {
+            return "Lose"
+        } else {
+            return "Draw"
+            }
+}
+console.log(computerSelection)
+const playerSelection = "rock"
+console.log(playRound(playerSelection, computerSelection))
