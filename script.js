7c37279f973450604ec4178b9d05b63b9aa1c30d
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-05 16:59:26 +1100

    computer selection done

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ function getComputerChoice() {
     const rand = Math.floor(Math.random() * compChoice.length)
 
     // Computer random choice
-    const compRandChoi = compChoice[rand]
-    console.log(compRandChoi)
+    const compRanChoi = compChoice[rand]
+    return compRanChoi
 }
-console.log(getComputerChoice())
+const computerSelection = getComputerChoice()
+console.log(computerSelection)
+
+const rockChoice = document.querySelector(".rock");
+rockChoice.addEventListener("click", myFunction);
+const paperChoice = document.querySelector("paper")
+const scissorsChoice = document.querySelector("scissors")
+
+function myFunction() {
+    console.log("hi")
+}
