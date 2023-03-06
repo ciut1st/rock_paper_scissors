424e2a29842767ec96362d564c3e442122b99fce
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-06 18:30:14 +1100

    fixing 1st round did not count

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function getComputerChoice() {
     return compChoice[rand]
 }
 
-
-
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -14,41 +12,45 @@ buttons.forEach(button => {
         playRound(playerSelection, computerSelection)
     })
 })
-
 let playerScore = 0
 let computerScore = 0
 let round = 0
-
-const result = document.querySelector('.result')
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection == "scissors"
     || playerSelection == "paper" && computerSelection == "rock"
     || playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore++
+        round++
         return playerWin()
     } else if (
         playerSelection == "rock" && computerSelection == "paper"
         || playerSelection == "paper" && computerSelection == "scissors"
         || playerSelection == "scissors" && computerSelection == "rock") {
+            computerScore++
+            round++
             return computerWin()
         } else {
+            round++
             return draw()
             }
 }
 
+const result = document.querySelector('.result')
+
 function playerWin() {
     result.innerHTML = "You win"
-    document.querySelector("#player-score-number").innerHTML = playerScore++
-    document.querySelector("#round-number").innerHTML = round++
+    document.querySelector("#player-score-number").innerHTML = playerScore
+    document.querySelector("#round-number").innerHTML = round
 }
 
 function computerWin() {
     result.innerHTML = "You lose"
-    document.querySelector("#computer-score-number").innerHTML = computerScore++
-    document.querySelector("#round-number").innerHTML = round++
+    document.querySelector("#computer-score-number").innerHTML = computerScore
+    document.querySelector("#round-number").innerHTML = round
 }
 
 function draw() {
     result.innerHTML = "It's a draw"
-    document.querySelector("#round-number").innerHTML = round++
+    document.querySelector("#round-number").innerHTML = round
 }
